5930eab1fa22462abf4765249a146f6c8c41cd25
digi <pataponis@yandex.ru>
2023-09-13 11:56:42 +0300

    mole game, change added

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -4,6 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
     let missCurrentCounter = document.querySelector('#lost');
     let killCounter = 0;
     let missCounter = 0;
+    let start = function() {
+        killCounter = 0;
+        missCounter = 0;
+        killCurrentCounter.textContent = killCounter;
+        missCurrentCounter.textContent = missCounter;
+    };
 
     field.addEventListener('click', (e) => {
         let target = e.target;
@@ -11,9 +17,17 @@ document.addEventListener('DOMContentLoaded', () => {
         if(target && target.classList.contains('hole_has-mole')) {
             killCounter++;
             killCurrentCounter.textContent = killCounter;
+            if(killCounter === 10) {
+                alert('Вы победили!');
+                start();
+            }
         } else if(target && target.classList.contains('hole') && !target.classList.contains('hole_has-mole')) {
             missCounter++;
             missCurrentCounter.textContent = missCounter;
+            if(missCounter === 5) {
+                alert('Вы проиграли!');
+                start();
+            }
         }
     });
 });
